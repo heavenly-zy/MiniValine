717233be5c29955b4bea9b43f019980a6f154987
zhaoyang <zhaoyang@un-net.com>
2021-02-02 11:48:13 +0800

    修改邮箱正则校验规则

--- src/utils/plugins/check.js
+++ src/utils/plugins/check.js
@@ -1,7 +1,7 @@
 const check = {
   mail (m) {
     return {
-      k: /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(m),
+      k: /^([A-Za-z0-9_\-\.\u4e00-\u9fa5])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,8})$/.test(m),
       v: m
     }
   },
